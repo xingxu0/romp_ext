8bddc00b24f30a027b5354a77c6421cd99af3be8
xingxux <vess527@gmail.com>
2015-10-23 14:59:43 -0700

    a

--- hist_binary.py
+++ hist_binary.py
@@ -1,6 +1,8 @@
 import lib, glob, commands, os, operator, pickle
+import matplotlib.pyplot as plt
 
 folder = "../image/5k_75"
+#folder = "../image/tecnick_1200_75"
 fs = glob.glob(folder + "/*.jpg")
 
 mask = 16
@@ -15,6 +17,7 @@ def encode(s):
 	return r
 
 res = {}
+total = 0
 for f in fs:
 	print f
 	if not os.path.isfile(f + ".block"):
@@ -29,13 +32,47 @@ for f in fs:
 		
 		ss = s[1][1:].split(" ")
 		c = encode(ss)
+		total += 1
 		if c in res:
 			res[c] += 1
 		else:
 			res[c] = 1
-		
+
+print total
 sort = sorted(res.items(), key=operator.itemgetter(1), reverse = True)
+
+x = []
+y = []
+yy = []
+
+i = 0
+now = 0
+for o in sort:
+	i += 1
+	x.append(i)
+	now += o[1]
+	y.append(now*1.0/total)
+	yy.append(o[1]*1.0/total)
+
+ax = plt.subplot()
+ax.plot(x, y)
+ax.set_xlim([1, 65536])
+plt.xlabel('Patterns')
+plt.ylabel('CDF (1)')
+plt.savefig('CDF.png')
+plt.close()
+
+ax = plt.subplot()
+ax.scatter(x, yy, s=1)
+ax.set_xlim([1, 65536])
+plt.xlabel('Patterns')
+plt.ylabel('PDF (1)')
+plt.savefig('PDF.png')
+
+	
+
 print sort[:100]
 print res["0000000000000000"]
-pickle.dump(sort, "hist_binary.pickle")
+output = open("hist_binary__.pickle", "wb")
+pickle.dump(sort, output)
 
--- lib.py
+++ lib.py
@@ -1,3 +1,4 @@
+# before function converts actual coefficient to magnitude (bits requried to represent this coefficient)
 def get_bits(i_num):
 	num = abs(i_num)
 	if num == 0:
